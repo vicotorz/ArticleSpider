50b48b790b1b1eae11162f03004509b3d75f9153
Victorz <victorzsnail@qq.com>
2017-11-20 18:19:25 +0800

    加入Request,urllib,item，Pillow，以及Item配置

--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -1,11 +1,96 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
+from scrapy.http import Request
+from urllib import parse
+import datetime
+from ArticleSpider.Utils.Common import get_md5
 
+#引入item进行值的填充
+from ArticleSpider.items import JobboleArticleItem
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/']
+    start_urls = ['http://blog.jobbole.com/all-posts/']
 
+    # 深度优先
     def parse(self, response):
+        # 1.所有url，每个页面进行解析（使用Request交给scrapy进行处理）
+        post_nodes = response.css("#archive .floated-thumb .post-thumb a")
+        for post_node in post_nodes:
+            image_url = post_node.css("img::attr(src)").extract_first("")  # 图片url
+            post_url = post_node.css("::attr(href)").extract_first("")  # post的url
+            yield Request(url=parse.urljoin(response.url, post_url), meta={"front_image_url": image_url},
+                          callback=self.parse_detail)
+
+            # 2.下一页url解析
+            next_url = response.css(".next.page-numbers::attr(href)").extract_first("")
+            if next_url:
+                yield Request(url=parse.urljoin(response.url, next_url), callback=self.parse)
+
+    def parse_detail(self, response):
+
+        # #xpath 语法爬虫
+        # #使用response
+        # title = response.xpath('//div[@class="entry-header"]/h1/text()').extract_first("")
+        # create_date = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()").extract()[0].strip().replace("·",
+        #                                                                                                             "").strip()
+        # praise_nums = response.xpath("//span[contains(@class, 'vote-post-up')]/h10/text()").extract()[0]
+        # fav_nums = response.xpath("//span[contains(@class, 'bookmark-btn')]/text()").extract()[0]
+        # match_re = re.match(".*?(\d+).*", fav_nums)
+        # if match_re:
+        #     fav_nums = match_re.group(1)
+        #
+        # comment_nums = response.xpath("//a[@href='#article-comment']/span/text()").extract()[0]
+        # match_re = re.match(".*?(\d+).*", comment_nums)
+        # if match_re:
+        #     comment_nums = match_re.group(1)
+        #
+        # content = response.xpath("//div[@class='entry']").extract()[0]
+        #
+        # tag_list = response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()
+        # tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
+        # tags = ",".join(tag_list)
+        article_item = JobboleArticleItem()
+        # 通过css选择器提取字段
+        front_image_url = response.meta.get("front_image_url", "")  #文章封面图
+        title = response.css(".entry-header h1::text").extract()[0]
+        create_date = response.css("p.entry-meta-hide-on-mobile::text").extract()[0].strip().replace("·","").strip()
+        praise_nums = response.css(".vote-post-up h10::text").extract()[0]
+        fav_nums = response.css(".bookmark-btn::text").extract()[0]
+        match_re = re.match(".*?(\d+).*", fav_nums)
+        if match_re:
+            fav_nums = int(match_re.group(1))
+        else:
+            fav_nums = 0
+
+        comment_nums = response.css("a[href='#article-comment'] span::text").extract()[0]
+        match_re = re.match(".*?(\d+).*", comment_nums)
+        if match_re:
+            comment_nums = int(match_re.group(1))
+        else:
+            comment_nums = 0
+
+        content = response.css("div.entry").extract()[0]
+
+        tag_list = response.css("p.entry-meta-hide-on-mobile a::text").extract()
+        tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
+        tags = ",".join(tag_list)
+
+        article_item["url_object_id"] = get_md5(response.url)
+        article_item["title"] = title
+        article_item["url"] = response.url
+        try:
+            create_date = datetime.datetime.strptime(create_date, "%Y/%m/%d").date()
+        except Exception as e:
+            create_date = datetime.datetime.now().date()
+        article_item["create_date"] = create_date
+        article_item["front_image_url"] = [front_image_url]
+        article_item["praise_nums"] = praise_nums
+        article_item["comment_nums"] = comment_nums
+        article_item["fav_nums"] = fav_nums
+        article_item["tags"] = tags
+        article_item["content"] = content
+
         pass
--- main.py
+++ main.py
@@ -7,5 +7,5 @@ import os
 from scrapy.cmdline import execute
 
 #可调式作用
-sys.path.append(os.path.dirname())
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 execute(["scrapy","crawl","jobbole"])
